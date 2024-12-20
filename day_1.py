59d89eff2e031b8c8300bf55cf4b05f955381b88
DCMill <77583400+DCMill@users.noreply.github.com>
2024-12-20 15:50:40 -0600

    Fixing day 1 code
    Just some changes to the file name for the input file and also changing the function use

--- day_1.py
+++ day_1.py
@@ -1,6 +1,6 @@
 
 def get_numbers():
-    with open("input.txt", "r") as input:
+    with open("day_1_input.txt", "r") as input:
         string = input.readlines()
         numbers = []
         for line in string:
@@ -12,20 +12,12 @@ def get_numbers():
         sorted_pairs = zip(sorted_list1, sorted_list2)
         return sorted_pairs, numbers
 def task1():
-    with open("input.txt", "r") as input:
-        string = input.readlines()
-        numbers = []
-        for line in string:
-            numbers.append((int(line.split("   ")[0]),int(line.split("   ")[1])))
-        print(numbers)
-        numbers = list(zip(*numbers))
-        sorted_list1 = sorted(numbers[0])
-        sorted_list2 = sorted(numbers[1])
-        sorted_pairs = zip(sorted_list1, sorted_list2)
-        distance = 0
-        for pair in sorted_pairs:
-            distance += abs(pair[1] - pair[0])
-        print(distance)
+    sorted_pairs,numbers = get_numbers()
+    distance = 0
+    for pair in sorted_pairs:
+        distance += abs(pair[1] - pair[0])
+    return distance
+print(task1())
 def task2():
     sorted, normal = get_numbers()
     similarity = 0
